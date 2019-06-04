5b603f6d216066f3b7c52ad16b0c2fd59046fddc
Nick Garner <nrgarner@ncsu.edu>
2019-06-04 10:38:55 -0400

    Created test files for Student and StudentRecordIO

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,11 @@
+package edu.ncsu.csc216.pack_scheduler.io;
+
+/**
+ * Tests StudentRecordIO class.
+ * 
+ * @author Nick Garner
+ *
+ */
+public class StudentRecordIOTest {
+
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
@@ -0,0 +1,11 @@
+package edu.ncsu.csc216.pack_scheduler.user;
+
+/**
+ * Tests Student class.
+ * 
+ * @author Nick Garner
+ *
+ */
+public class StudentTest {
+
+}
