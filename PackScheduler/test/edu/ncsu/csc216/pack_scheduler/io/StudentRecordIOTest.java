0b6eece42b42da778495c43e3d18aff64d27c430
Nick Garner <nrgarner@ncsu.edu>
2019-06-11 13:47:35 -0400

    Implemented setUp method with pw hashing in StudentRecordIOTest

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -2,7 +2,7 @@ package edu.ncsu.csc216.pack_scheduler.io;
 
 import static org.junit.Assert.*;
 
-//import org.junit.Before;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.ncsu.csc216.collections.list.SortedList;
@@ -10,6 +10,8 @@ import edu.ncsu.csc216.pack_scheduler.user.Student;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Tests StudentRecordIO class.
@@ -25,24 +27,45 @@ public class StudentRecordIOTest {
 	private final String invalidTestFile = "test-files/invalid_student_records.txt";
 
 	/** Expected results for valid student records */
-	private final String validStudent1 = "Demetrius,Austin,daustin,Curabitur.egestas.nunc@placeratorcilacus.co.uk,0�R��\"��u���\\7X�F���9�{-O�F�ap�,18";
-	private final String validStudent2 = "Lane,Berg,lberg,sociis@non.org,0�R��\"��u���\\7X�F���9�{-O�F�ap�,14";
-	private final String validStudent3 = "Raymond,Brennan,rbrennan,litora.torquent@pellentesquemassalobortis.ca,0�R��\"��u���\\7X�F���9�{-O�F�ap�,12";
-	private final String validStudent4 = "Emerald,Frost,efrost,adipiscing@acipsumPhasellus.edu,0�R��\"��u���\\7X�F���9�{-O�F�ap�,3";
-	private final String validStudent5 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,0�R��\"��u���\\7X�F���9�{-O�F�ap�,14";
-	private final String validStudent6 = "Althea,Hicks,ahicks,Phasellus.dapibus@luctusfelis.com,0�R��\"��u���\\7X�F���9�{-O�F�ap�,11";
-	private final String validStudent7 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,0�R��\"��u���\\7X�F���9�{-O�F�ap�,15";
-	private final String validStudent8 = "Dylan,Nolan,dnolan,placerat.Cras.dictum@dictum.net,0�R��\"��u���\\7X�F���9�{-O�F�ap�,5";
-	private final String validStudent9 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,0�R��\"��u���\\7X�F���9�{-O�F�ap�,4";
-	private final String validStudent10 = "Griffith,Stone,gstone,porta@magnamalesuadavel.net,0�R��\"��u���\\7X�F���9�{-O�F�ap�,17";
+	private final String validStudent1 = "Demetrius,Austin,daustin,Curabitur.egestas.nunc@placeratorcilacus.co.uk,pw,18";
+	private final String validStudent2 = "Lane,Berg,lberg,sociis@non.org,pw,14";
+	private final String validStudent3 = "Raymond,Brennan,rbrennan,litora.torquent@pellentesquemassalobortis.ca,pw,12";
+	private final String validStudent4 = "Emerald,Frost,efrost,adipiscing@acipsumPhasellus.edu,pw,3";
+	private final String validStudent5 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,pw,14";
+	private final String validStudent6 = "Althea,Hicks,ahicks,Phasellus.dapibus@luctusfelis.com,pw,11";
+	private final String validStudent7 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,pw,15";
+	private final String validStudent8 = "Dylan,Nolan,dnolan,placerat.Cras.dictum@dictum.net,pw,5";
+	private final String validStudent9 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,pw,4";
+	private final String validStudent10 = "Griffith,Stone,gstone,porta@magnamalesuadavel.net,pw,17";
 
 	/** Array to hold expected results */
 	private final String[] validStudents = { validStudent1, validStudent2, validStudent3, validStudent4, validStudent5,
 			validStudent6, validStudent7, validStudent8, validStudent9, validStudent10 };
+	
+	/** Student's password after it has been hashed with SHA_256 */
+	private String hashPW;
+	/** Algorithm to use for hashing Student passwords */
+	private static final String HASH_ALGORITHM = "SHA-256";
 
-//	@Before
-//	public void setUp() throws Exception {
-//	}
+	/**
+	 * Converts plain-text password for each Student to a hashed password hashPW
+	 * @throws Exception When hashing algorithm cannot be found.
+	 */
+	@Before
+	public void setUp() throws Exception {
+		try {
+			String password = "pw";
+			MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM);
+			digest.update(password.getBytes());
+			hashPW = new String(digest.digest());
+			
+			for (int i = 0; i < validStudents.length; i++) {
+				validStudents[i] = validStudents[i].replace(",pw,", "," + hashPW + ",");
+			}
+		} catch (NoSuchAlgorithmException e) {
+			fail("Unable to create hash during setup");
+		}
+	}
 
 	/**
 	 * Tests readValidCourseRecords().
