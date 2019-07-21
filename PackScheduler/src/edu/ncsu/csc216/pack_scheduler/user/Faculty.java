d94573dd68770f07242ed3bc6c456a82504b24d1
Nick Garner <nrgarner@ncsu.edu>
2019-07-21 17:49:17 -0400

    Finished Faculty implementation, implemented FacultyTest

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Faculty.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Faculty.java
@@ -1,26 +1,63 @@
 package edu.ncsu.csc216.pack_scheduler.user;
 
+/**
+ * Class defines state and behavior for Faculty objects. Faculty are a subclass
+ * of Users and have the additional field maxCourses, denoting how many courses
+ * the Faculty can teach per term.
+ * 
+ * @author Nick Garner
+ *
+ */
 public class Faculty extends User {
 
+	/** Maximum number of courses the Faculty member can teach */
 	private int maxCourses;
+	/** Smallest maxCourses number a Faculty can have */
 	public static final int MIN_COURSES = 1;
+	/** Largets maxCourses number a Faculty can have */
 	public static final int MAX_COURSES = 3;
-	
-	public Faculty(String firstName, String lastName, String id, String email, String password) {
+
+	/**
+	 * Constructs a new Faculty object
+	 * 
+	 * @param firstName First name to set
+	 * @param lastName  Last name to set
+	 * @param id        Unity ID to set
+	 * @param email     Email address to set
+	 * @param password  Plain text password to set
+	 * @param maxCourses Max courses to set
+	 */
+	public Faculty(String firstName, String lastName, String id, String email, String password, int maxCourses) {
 		super(firstName, lastName, id, email, password);
+		setMaxCourses(maxCourses);
 	}
-	
+
+	/**
+	 * Sets maxCourses to the given int param
+	 * 
+	 * @param maxCourses The maximum number of courses to set for maxCourses
+	 */
 	public void setMaxCourses(int maxCourses) {
-		this.maxCourses = maxCourses;
-	}
-	
-	public int getMaxCourses() {
 		if (maxCourses < 1 || maxCourses > 3) {
 			throw new IllegalArgumentException("Maximum Courses must be between 1 and 3 inclusive.");
 		}
+		this.maxCourses = maxCourses;
+	}
+
+	/**
+	 * Returns the maximum courses this Faculty member can teach
+	 * 
+	 * @return Int representing the maximum courses this Faculty member can teach
+	 */
+	public int getMaxCourses() {
 		return maxCourses;
 	}
 
+	/**
+	 * Ensures that equal objects hash to the same value
+	 * 
+	 * @return Hash value as an int
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -29,6 +66,12 @@ public class Faculty extends User {
 		return result;
 	}
 
+	/**
+	 * Returns true if this Faculty object matches the parameter object
+	 * 
+	 * @param obj The object to compare to this Faculty object
+	 * @return True if matches param object
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -42,6 +85,17 @@ public class Faculty extends User {
 			return false;
 		return true;
 	}
-	
-	
+
+	/**
+	 * Returns a String representation of this Faculty object with name, Unity ID,
+	 * email, password, and maximum number of courses.
+	 * 
+	 * @return String representation of the Faculty object's field data
+	 */
+	@Override
+	public String toString() {
+		return this.getFirstName() + "," + this.getLastName() + "," + this.getId() + "," + this.getEmail() + ","
+				+ this.getPassword() + "," + this.getMaxCourses();
+	}
+
 }
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/FacultyTest.java
@@ -0,0 +1,108 @@
+package edu.ncsu.csc216.pack_scheduler.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests Faculty class for proper functionality and exception handling.
+ * 
+ * @author Nick Garner
+ *
+ */
+public class FacultyTest {
+
+	/** Test first name */
+	private static final String FIRST_NAME = "Charles";
+	/** Test last name */
+	private static final String LAST_NAME = "Xavier";
+	/** Test id */
+	private static final String ID = "cxavier";
+	/** Test email */
+	private static final String EMAIL = "cxavier@ncsu.edu";
+	/** Test password */
+	private static final String PASSWORD = "pw";
+	/** Test max courses */
+	private static final int MAX_COURSES = 1;
+	/** Faculty test object */
+	private Faculty f;
+
+	/**
+	 * Runs before each test to instantiate a Faculty object for testing
+	 * 
+	 * @throws Exception If error occurs during setup
+	 */
+	@Before
+	public void setUp() throws Exception {
+		f = new Faculty(FIRST_NAME, LAST_NAME, ID, EMAIL, PASSWORD, MAX_COURSES);
+	}
+
+	/**
+	 * Tests Faculty constructor for proper setting of fields
+	 */
+	@Test
+	public void testFaculty() {
+		assertEquals(FIRST_NAME, f.getFirstName());
+		assertEquals(LAST_NAME, f.getLastName());
+		assertEquals(ID, f.getId());
+		assertEquals(PASSWORD, f.getPassword());
+		assertEquals(MAX_COURSES, f.getMaxCourses());
+	}
+
+	/**
+	 * Tests that setMaxCourses properly throws exception for value out of bounds
+	 * and properly changes maxCourses field
+	 */
+	@Test
+	public void testSetMaxCourses() {
+		// Bad values
+		try {
+			f.setMaxCourses(0);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Maximum Courses must be between 1 and 3 inclusive.", e.getMessage());
+		}
+		try {
+			f.setMaxCourses(4);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Maximum Courses must be between 1 and 3 inclusive.", e.getMessage());
+		}
+		// Good value
+		f.setMaxCourses(2);
+		assertEquals(2, f.getMaxCourses());
+	}
+
+	/**
+	 * Tests that toString returns the proper String representation of the Faculty
+	 * object
+	 */
+	@Test
+	public void testToString() {
+		assertEquals(FIRST_NAME + "," + LAST_NAME + "," + ID + "," + EMAIL + "," + PASSWORD + "," + MAX_COURSES,
+				f.toString());
+	}
+
+	/**
+	 * Tests that identical objects hash to the same value
+	 */
+	@Test
+	public void testHashCode() {
+		Faculty compare = new Faculty(FIRST_NAME, LAST_NAME, ID, EMAIL, PASSWORD, MAX_COURSES);
+		assertEquals(compare.hashCode(), f.hashCode());
+	}
+
+	/**
+	 * Tests that equals returns True for identical objects
+	 */
+	@Test
+	public void testEqualsObject() {
+		Faculty compare = new Faculty(FIRST_NAME, LAST_NAME, ID, EMAIL, PASSWORD, MAX_COURSES);
+		assertTrue(f.equals(compare));
+		compare.setMaxCourses(2);
+		assertFalse(f.equals(compare));
+		assertFalse(f.equals(null));
+	}
+
+}
